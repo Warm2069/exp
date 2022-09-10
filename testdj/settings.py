77d14ea592f9414d30f0be1c4f3dfcf1ffb94148
Sagar Sharma <sagar.wiz91@gmail.com>
2022-09-10 23:17:39 +0530

    first commit

--- testdj/settings.py
+++ testdj/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '_^-12pz5$qj_8_r*q$5oia-r+@n$p85qobzpg!j^sr)icl=xme'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["image2346.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
